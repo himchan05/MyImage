110f3689a4ee8423da4b77f4a757e8c0cf609cc9
박힘찬 <himchan05@naver.com>
2022-03-06 21:54:41 +0900

    Initial Commit

---
+++ MyImage/MyImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyImageApp.swift
+//  MyImage
+//
+//  Created by 박힘찬 on 2022/03/06.
+//
+
+import SwiftUI
+
+@main
+struct MyImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
